ee3f384c227a9517a1aef0c9e5d5e73672230c22
Husseinm <husseinm1376@gmail.com>
2026-07-24 16:48:18 +0330

    reshape_data_for_sklean

--- model.py
+++ model.py
@@ -4,6 +4,20 @@ def model_exist(model_file):
     import os
     return(os.path.exists(model_file))
 
+def reshape_data_for_sklearn(rows):
+
+    x = []
+    y = []
+
+    for row in rows:
+        population = row[3]
+        area = row[4]
+
+        x.append([population])
+        y.append(area)
+    
+    return x,y
+
 
 def train_model(X,Y):
 
