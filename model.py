7a4761af58049e14fe2744554d96231595a02f53
Husseinm <husseinm1376@gmail.com>
2026-07-24 16:32:06 +0330

    add train_model def to model.py module

--- model.py
+++ model.py
@@ -1,10 +1,15 @@
+from sklearn.linear_model import LinearRegression
+
 def model_exist(model_file):
     import os
     return(os.path.exists(model_file))
 
 
-def train_model():
-    pass
+def train_model(X,Y):
+
+    model = LinearRegression()
+    model.fit(X,Y)
+    return model
 
 def save_model():
     pass
